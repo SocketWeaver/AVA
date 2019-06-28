e4ddff8f46baf9251f780bd79c85410cc666d782
peter <peterzhou43@gmail.com>
2019-06-28 01:00:46 -0700

    Part 3

--- Final/Assets/Scripts/Player/PlayerWeapon.cs
+++ Final/Assets/Scripts/Player/PlayerWeapon.cs
@@ -1,42 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using SWNetwork;
 
 public class PlayerWeapon : MonoBehaviour
 {
-    public Constants.Guns DeafultGunType = Constants.Guns.Laser;
+    public Constants.Guns DefaultGunType = Constants.Guns.Laser;
 
     public List<Gun> Guns;
 
     Player player;
     Gun curretnGun;
 
+    NetworkID networkID;
+    SyncPropertyAgent syncPropertyAgent;
+
+    const string CURRENT_WEAPON_INDEX = "CurrentWeaponIndex";
+
+
     private void Start()
     {
         player = GetComponent<Player>();
-        ActivateWeapon(DeafultGunType);
+        ActivateWeapon(DefaultGunType);
+        networkID = GetComponent<NetworkID>();
+        syncPropertyAgent = GetComponent<SyncPropertyAgent>();
     }
 
     void Update()
     {
-        if (player.Dead)
+        if (player.Dead || !networkID.IsMine)
         {
             return;
         }
 
         if (Input.GetKeyUp(KeyCode.Alpha1))
         {
-            ActivateWeapon(Constants.Guns.Laser);
+            syncPropertyAgent.Modify(CURRENT_WEAPON_INDEX, (int)Constants.Guns.Laser);
+            //ActivateWeapon(Constants.Guns.Laser);
         }
         else if (Input.GetKeyUp(KeyCode.Alpha2))
         {
-            ActivateWeapon(Constants.Guns.MachineGun);
+            syncPropertyAgent.Modify(CURRENT_WEAPON_INDEX, (int)Constants.Guns.MachineGun);
+            //ActivateWeapon(Constants.Guns.MachineGun);
         }
 
         if (Input.GetButton("Fire1"))
         {
             curretnGun.Fire();
         }
+    }
+
+    public void OnCurrentWeaponIndexChanged()
+    {
+        Constants.Guns gunType = (Constants.Guns)syncPropertyAgent.GetPropertyWithName(CURRENT_WEAPON_INDEX).GetIntValue();
+        if(gunType == 0)
+        {
+            syncPropertyAgent.Modify(CURRENT_WEAPON_INDEX, (int)DefaultGunType);
+        }
+        ActivateWeapon(gunType);
     }
 
     public void ActivateWeapon(Constants.Guns gunType)
